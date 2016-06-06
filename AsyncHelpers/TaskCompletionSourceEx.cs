fa89033555f5305d05f0b3054fb97d55fa896a39
aryapolov <aryapolov@ntoire-polus.ru>
2016-06-06 15:38:12 +0300

    Added Propagate func and WithTimeout execution

---
+++ AsyncHelpers/TaskCompletionSourceEx.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AR.Helpers
+{
+    public static class TaskCompletionSourceEx
+    {
+        /// <summary>
+        /// Try set Task's result to TaskCompletionSource
+        /// </summary>
+        /// <typeparam name="T">Task' result type</typeparam>
+        /// <param name="tcs">TaskCompletionSource object</param>
+        /// <param name="task">Task to propagate</param>
+        public static void Propagate<T>(this TaskCompletionSource<T> tcs, Task<T> task)
+        {
+            if (task.Status == TaskStatus.Faulted)
+            {
+                var aggrEx = task.Exception;
+                if (aggrEx.InnerExceptions != null && aggrEx.InnerExceptions.Count > 0)
+                    tcs.TrySetException(aggrEx.InnerExceptions[0]);
+                else
+                    tcs.TrySetException(aggrEx);
+            }
+            else if (task.Status == TaskStatus.Canceled)
+            {
+                tcs.TrySetCanceled();
+            }
+            else if (task.Status == TaskStatus.RanToCompletion)
+            {
+                tcs.TrySetResult(task.Result);
+            }
+            else 
+            {
+                throw new InvalidOperationException(
+                    String.Format("Task should be completed. Current task status: {0}", task.Status));
+            }
+        }
+
+        /// <summary>
+        /// Try set Task's result to TaskCompletionSource
+        /// </summary>
+        /// <param name="tcs">TaskCompletionSource object</param>
+        /// <param name="task">Task to propagate</param>
+        public static void Propagate(this TaskCompletionSource<object> tcs, Task task)
+        {
+            if (task.Status == TaskStatus.Faulted)
+            {
+                var aggrEx = task.Exception;
+                if (aggrEx.InnerExceptions != null && aggrEx.InnerExceptions.Count > 0)
+                    tcs.TrySetException(aggrEx.InnerExceptions[0]);
+                else
+                    tcs.TrySetException(aggrEx);
+            }
+            else if (task.Status == TaskStatus.Canceled)
+            {
+                tcs.TrySetCanceled();
+            }
+            else if (task.Status == TaskStatus.RanToCompletion)
+            {
+                tcs.TrySetResult(null);
+            }
+            else
+            {
+                throw new InvalidOperationException(
+                    String.Format("Task should be completed. Current task status: {0}", task.Status));
+            }
+        }
+
+    }
+}
---
+++ AsyncHelpers/TaskEx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AR.Helpers
+{
+    public static class TaskEx
+    {
+        /// <summary>
+        /// Execute task with timeout
+        /// </summary>
+        /// <typeparam name="T">Task result type</typeparam>
+        /// <param name="task">Task</param>
+        /// <param name="timeout">Timeout value</param>
+        /// <returns></returns>
+        public static Task<T> WithTimeout<T>(this Task<T> task, TimeSpan timeout)
+        {
+            var tcs = new TaskCompletionSource<T>();
+
+            task.ContinueWith(t => tcs.Propagate(task), 
+                TaskContinuationOptions.ExecuteSynchronously);
+
+            Task.Delay(timeout).ContinueWith(t => tcs.TrySetCanceled(), 
+                TaskContinuationOptions.ExecuteSynchronously);
+
+            return tcs.Task;
+        }
+
+        /// <summary>
+        /// Execute task with timeout
+        /// </summary>
+        /// <param name="task">Task to execute</param>
+        /// <param name="timeout">Timeout value</param>
+        /// <returns></returns>
+        public static Task WithTimeout(this Task task, TimeSpan timeout)
+        {
+            var tcs = new TaskCompletionSource<object>();
+
+            task.ContinueWith(t => tcs.Propagate(task), 
+                TaskContinuationOptions.ExecuteSynchronously);
+
+            Task.Delay(timeout)
+                .ContinueWith(t => tcs.TrySetCanceled(),
+                    TaskContinuationOptions.ExecuteSynchronously);
+
+            return tcs.Task;
+        }
+    }
+}
