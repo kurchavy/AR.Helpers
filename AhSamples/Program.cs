bd5686c28f9de2a300ca027b820de3b3f7b9ba2b
aryapolov <aryapolov@ntoire-polus.ru>
2016-06-06 16:36:24 +0300

    Added some tests

--- AhSamples/Program.cs
+++ AhSamples/Program.cs
@@ -17,14 +17,17 @@ namespace AhSamples
 
         private void TestTimeout()
         {
-            var t1 = GetResult(1, TimeSpan.FromMilliseconds(1000));
-            var t2 = GetResult(2, TimeSpan.FromMilliseconds(2100));
+            var t1 = GetResult(TimeSpan.FromMilliseconds(2000));
+            var t2 = GetResult(TimeSpan.FromMilliseconds(5000));
+            var t3 = GetResult(TimeSpan.FromMilliseconds(5000), 2);
+
+            t2.ContinueWith(t => Console.WriteLine(t.Result));
 
             var r1 = Task.Run(async () =>
             {
                 try
                 {
-                    await t1.WithTimeout(TimeSpan.FromSeconds(2));
+                    await t1.WithTimeout(TimeSpan.FromSeconds(3));
                     Console.WriteLine(t1.Result);
                 }
                 catch (OperationCanceledException)
@@ -41,7 +44,7 @@ namespace AhSamples
             {
                 try
                 {
-                    await t2.WithTimeout(TimeSpan.FromSeconds(2));
+                    await t2.WithTimeout(TimeSpan.FromSeconds(3));
                     Console.WriteLine(t2.Result);
                 }
                 catch (OperationCanceledException)
@@ -53,16 +56,40 @@ namespace AhSamples
                     Console.WriteLine(ex);
                 }
             });
-            
+
+            var r3 = Task.Run(async () =>
+            {
+                try
+                {
+                    await t3.WithTimeout(TimeSpan.FromSeconds(3));
+                    Console.WriteLine(t3.Result);
+                }
+                catch (OperationCanceledException)
+                {
+                    Console.WriteLine("Task T3 cancelled");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            });
+
             Console.ReadLine();
         }
 
 
 
-        private async Task<string> GetResult(int arg, TimeSpan ts)
+        private async Task<string> GetResult(TimeSpan ts, int throwEx = 0)
         {
+            if (throwEx == 1)
+                throw new ArgumentException("Boom before delay!!!");
+
             await Task.Delay(ts);
-            return "Result is " + arg.ToString();
+
+            if (throwEx == 2)
+                throw new ArgumentException("Boom after delay!!!");
+
+            return "Result is " + ts.ToString();
         }
     }
 }
