9a90e708c43bd87c08e73b6d309850e480008d1e
aryapolov <aryapolov@ntoire-polus.ru>
2016-06-06 14:58:33 +0300

    Added AR.WPF.Helpers library 1.0.0

---
+++ WpfBaseHelpers/BindableBase.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace AR.WPF.Helpers
+{
+    /// <summary>
+    /// Base class that implements INPC
+    /// </summary>
+    public class BindableBase : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Set property value
+        /// </summary>
+        /// <typeparam name="T">Underlying field type</typeparam>
+        /// <param name="member">Field to set</param>
+        /// <param name="val">Value</param>
+        /// <param name="propertyName">Name of property</param>
+        /// <returns></returns>
+        protected virtual bool SetProperty<T>(ref T member, T val,
+            [CallerMemberName] string propertyName = null)
+        {
+            if (object.Equals(member, val)) return false;
+
+            member = val;
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            return true;
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+        /// <summary>
+        /// INPC Implementation
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+    }
+}
---
+++ WpfBaseHelpers/BitmapSourceConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Interop;
+using System.Windows.Media.Imaging;
+
+namespace AR.WPF.Graphics
+{
+    public static class BitmapSourceConvert
+    {
+        /// <summary>
+        /// Delete a GDI object
+        /// </summary>
+        /// <param name="o"></param>
+        /// <returns></returns>
+        [DllImport("gdi32")]
+        private static extern int DeleteObject(IntPtr o);
+
+        /// <summary>
+        /// Convert Bitmap to a WPF BitmapSource. 
+        /// </summary>
+        /// <param name="image">Source bitmap</param>
+        /// <returns>BitmapSource</returns>
+        public static BitmapSource ToBitmapSource(Bitmap image)
+        {
+            using (var source = image)
+            {
+                IntPtr ptr = source.GetHbitmap(); 
+                BitmapSource bs = Imaging.CreateBitmapSourceFromHBitmap(ptr, IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+                DeleteObject(ptr); 
+                return bs;
+            }
+        }
+    }
+}
