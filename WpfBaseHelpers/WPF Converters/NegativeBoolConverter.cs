a2191dc8f308e37928002282baabfe0b01cab4e3
aryapolov <aryapolov@ntoire-polus.ru>
2016-06-16 09:45:11 +0300

    Added NegativeBoolConverter

---
+++ WpfBaseHelpers/WPF Converters/NegativeBoolConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace AR.WPF.Helpers
+{
+    /// <summary>
+    /// Converts bool to negative value
+    /// </summary>
+    public class NegativeBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool res = (bool)value;
+            return !res;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
