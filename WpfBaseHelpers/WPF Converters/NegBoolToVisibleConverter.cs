d9741503c6dc535e5a4b99340d60d22c19118b69
aryapolov <aryapolov@ntoire-polus.ru>
2016-07-20 16:03:39 +0300

    Added NegBoolToVisibilityConverter

---
+++ WpfBaseHelpers/WPF Converters/NegBoolToVisibleConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace AR.WPF.Helpers
+{
+    /// <summary>
+    /// Converts negative bool to Visibility
+    /// </summary>
+    public class NegBoolToVisibleConverter : IValueConverter
+    {
+        #region IValueConverter Members
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool bValue = (bool)value;
+            if (!bValue)
+                return Visibility.Visible;
+            else
+                return Visibility.Hidden;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
